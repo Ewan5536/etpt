f1d02ed41fc71af8b52fa11a3e947392d9550e41
ewan thomas <ewan5536@gmail.com>
2020-02-27 20:04:00 +0000

    exersice layout for training goals

--- src/pages/Endurance.js
+++ src/pages/Endurance.js
@@ -1,8 +1,12 @@
 import React from "react";
 import endurance from '../images/leghops.gif';
-import Plank from '../images/plank.gif';
-import incline from '../images/inclinePress.gif';
-import DoorRow from '../images/doorRow.gif';
+import montClimb from '../images/mountain-climbers.gif';
+import twisit from '../images/twist-sits.gif';
+import squatjump from '../images/squatjump.gif';
+import pressUp from '../images/pressUps.gif';
+import Hipthrust from '../images/SupineBridge.gif';
+import wlklegstretch from '../images/walkLegStr.gif';
+import widepress from '../images/widePushup.gif';
 
 import { Link } from 'react-router-dom';
 
@@ -11,71 +15,110 @@ function Endurance() {
 
     return (
         <div>
-            <div className="topic">
-                <div className="item">
 
-
-                    <img src={Plank} alt='over-head press' />
-
-                    <div className="overlay">
-
-                        <div className='overlayText'>
-                            SET-UP: Set up on the floor resting on your elbows and toes.
-                        Place your elbows in line with your shoulders, and form a straight line between your feet, hips and shoudlers.<br />
-                            LIFT: Brace through the core and maintain proper posture throughout.<br />
-                            EMPHASIS: Do not allow the hips the sag, or the butt to flare up above the back.
-                            Keep the shoulder blades back and together. It may be helpful to visualize proper standing posture, only your body is rotated 90 degrees toward the floor.
-                        </div>
-                        {/*https://www.youtube.com/watch?v=KKPTt8TJWIs&feature=emb_title*/}
-                    </div>
-                </div>
-                {/* routing from homepage */}
-                <div className="item">
-
-
-                    <img src={incline} alt='incline press' />
-
-                    <div className="overlay">
-                        <div className='overlayText'>
-                            SET-UP: Find a surface to elevate your feet on.
-                            With the legs together and hips up, establish proper upper body posture with a neutral back.
-                            Place your hands on the floor at slightly wider than shoulder width and fingers pointing forward.<br />
-                            MOVEMENT: Press the chest off the floor until the arms are straight, then return to the starting position.
-                            Cue the descent by attempting to 'pull' yourself down to the floor with your shoulder blades.<br />
-                            EMPHASIS: Don't allow the hips or the lower back to sag. Use a complete range of motion, bringing the chest to the floor.
-                            Don't push the head forward to the floor.
-                    </div>
-                        {/* https://www.youtube.com/watch?v=SKPab2YC8BE&feature=emb_title */}
-                    </div>
-                </div>
+            <div>
                 <div className="topic">
-                    <div className="item">
-                        <img src={DoorRow} alt='single arm door row' />
+                    <div className="item"><img src={montClimb} alt='stride jumps' />
                         <div className="overlay">
-                            SET-UP: Begin in front of a sturdy door frame.
-                        Establish a shoulder width or slightly wider stance and grasp the doorway with one arm so that the wall is perpendicular to your arm.<br />
-                            LIFT: Now slowly sit back using your arm to maintain your upright position and to keep you from falling down.
-                            Once you have a suitable resistance in your arm, simply pull yourself into the doorframe.
-                        Go slowly and do not use momentum.<br />
-                            REPETITIONS: Perform all prescribed reps on one side, then on the other.
-                            This counts as one set.
+                            <div className='overlayText'>
+                                MOVEMENT: Start on all fours and pike your hips up so that your feet are on the ground, and your arms and legs are straight. 
+                                Now simply lift one leg up so that your knee is at chest height. 
+                                Quickly switch legs and repeat in a rhythmic fashion. 
+                                It is critically important in this exercise to achieve range of motion in the hips and not to excessively flex the lumbar spine. 
+                                Start with a slow pace and short strides and increase speed and stride length as you progress.
+                                <br /> REPETITIONS: When both legs have come up to the chest, this counts as one repetition
+                           </div>
                         </div>
                     </div>
-                    <div className="item">
-
-                        <img src={endurance} alt='endurance' />
-                        <button className="overlay">
-                            ddhdhdhd dhdhdhdh dhdhdhd hdhdhdh dhdhdh dhh
-                            <br />gdgdgdgdgdgdgdg gdgdgd gdgdgd ggdg
-                            <br />dhdhdhhdhd
-                            </button>
-
+                    {/* routing from homepage */}
+                    <div className="item"><img src={twisit} alt='twist sit ups' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            MOVEMENT: To begin the sit-up start with feet flat on the ground and knees bent. 
+                            Contract your abdominals and raise your trunk up until it is vertical or nearly vertical to the ground. 
+                            Rotating through the middle and upper back, turn your body 45 degrees to the side, return to the center and repeat in the other direction. 
+                            Lower yourself under control and repeat.
+                             </div>
+                        </div>
                     </div>
-
                 </div>
             </div>
+            <div>
+                <div className="topic">
+                    <div className="item"><img src={squatjump} alt='on the spot squat jumps ' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            MOVEMENT: bring your knees towards your chest in squat action. Jump as high as you can and. Quickly revert your legs to land on your toes, and repeat the movement utilizing the stretch reflex on each jump.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item"><img src={pressUp} alt='the press up' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            SET-UP: Set up with your hands either side to the torso. 
+                            With the toes together and hips up, establish proper upper body posture with a neutral back.
+                            LIFT: Press the chest off the floor until the arms are straight, then return to the starting position. 
+                            Cue the descent by attempting to 'pull' yourself down to the floor with your shoulder blades.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="topic">
+                    <div className="item"><img src={Hipthrust} alt='hip thrusts' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            MOVEMENT: Lie on your back with knees bent and arms by your sides. 
+                            Tighten your abs and drive your hips up by contracting your glutes. 
+                            Extend until there is a straight line between your knees, hips and shoulders.
+                            Lower and repeat.
+                           </div>
+                        </div>
+                    </div>
+                    {/* routing from homepage */}
+                    <div className="item"><img src={widepress} alt=' wide arm press up' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            SET-UP: Set up with your hands slightly wider than shoulder width - so that the upper arms are tucked at 45-degree angle to the torso - and fingers pointing forward. 
+                            With the toes together and hips up, establish proper upper body posture with a neutral back. 
+                            LIFT: Press the chest off the floor until the arms are straight, then return to the starting position. 
+                            Cue the descent by attempting to 'pull' yourself down to the floor with your shoulder blades.
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="topic">
+                    <div className="item"><img src={wlklegstretch} alt='walking leg stretch' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            •	MOVEMENT: Lift your ankle behind you. 
+                            Keeping the knee next to your stabilizing leg, bring the ankle up to your buttocks until you feel a good stretch. 
+                            Keep the knee in a line with your torso and do not allow it to flare forward or outward. Hold for prescribed time. 
+                            Release and take a step forward. Repeat on the opposite leg. 
+                            •	REPETITIONS: Hold for the full prescribed time on one side, then switch sides and repeat. 
+                            This counts as one set.
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item"><img src={endurance} alt='side to side leg hopping' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                            •	MOVEMENT: Simply hop up and down, side to side on one leg.
+                            Land on the toes and not the heels and perform in a rhythmic fashion.
+                            •	REPETITIONS: Perform all prescribed reps on one side, then on the other. 
+                            This counts as one set.
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
         </div>
-    )
-}
+    )   
+}   
 
 export default Endurance;
--- src/pages/Flexability.js
+++ src/pages/Flexability.js
@@ -1,70 +1,107 @@
 import React from "react";
-import endurance from '../images/endurCard.gif';
 import Plank from '../images/plank.gif';
-import incline from '../images/inclinePress.gif';
-import DoorRow from '../images/doorRow.gif';
+import catCamel from '../images/cat_camel.gif';
+import sideTwist from '../images/sidetwists.gif';
+import wallReach from '../images/ScapularWallSlides.gif';
+import kneelRota from '../images/thoracicRotation.gif';
+import pecStretch from '../images/PecStretch.gif';
 
-import { Link } from 'react-router-dom';
+
+
 
 
 function Flexability() {
 
-    return (
+    return (<div>
+
         <div>
             <div className="topic">
-                <div className="item">
-                    <img src={Plank} alt='over-head press' />
-                        <div className="overlay">
-                            <div className='overlayText'>
-                                SET-UP: Set up on the floor resting on your elbows and toes.
-                                Place your elbows in line with your shoulders, and form a straight line between your feet, hips and shoudlers.<br />
-                                LIFT: Brace through the core and maintain proper posture throughout.<br />
-                                EMPHASIS: Do not allow the hips the sag, or the butt to flare up above the back.
-                                Keep the shoulder blades back and together. It may be helpful to visualize proper standing posture, only your body is rotated 90 degrees toward the floor.
-                            </div>
-                            {/*https://www.youtube.com/watch?v=KKPTt8TJWIs&feature=emb_title*/}
-                        </div>
-                </div>
-                {/* routing from homepage */}
-            <div className="item">
-                <img src={incline} alt='incline press' />
+                <div className="item"><img src={Plank} alt='the plank' />
                     <div className="overlay">
                         <div className='overlayText'>
-                            SET-UP: Find a surface to elevate your feet on.
-                            With the legs together and hips up, establish proper upper body posture with a neutral back.
-                            Place your hands on the floor at slightly wider than shoulder width and fingers pointing forward.<br />
-                            MOVEMENT: Press the chest off the floor until the arms are straight, then return to the starting position.
-                            Cue the descent by attempting to 'pull' yourself down to the floor with your shoulder blades.<br />
-                            EMPHASIS: Don't allow the hips or the lower back to sag. Use a complete range of motion, bringing the chest to the floor.
-                            Don't push the head forward to the floor.
+                            • SET-UP: Set up on the floor resting on your elbows and toes.
+                                Place your elbows in line with your shoulders, and form a straight line between your feet, hips and shoudlers.
+                            <br />• LIFT: Brace through the core and maintain proper posture throughout.
+                            <br />• EMPHASIS: Do not allow the hips the sag, or the butt to flare up above the back.
+                            Keep the shoulder blades back and together. It may be helpful to visualize proper standing posture, only your body is rotated 90 degrees toward the floor.
+                        </div>
+                        {/*https://www.youtube.com/watch?v=KKPTt8TJWIs&feature=emb_title*/}
+                    </div>
+                </div>
+                {/* routing from homepage */}
+                <div className="item"><img src={catCamel} alt='cat pose to camel pose' />
+                    <div className="overlay">
+                        <div className='overlayText'>
+                            • SET-UP: Start on all fours with your arms and thighs vertical.
+                        <br />• MOVEMENT: Start the movement by bringing your head and pelvis towards each other - effectively rounding your back (the 'camel' position).
+                             Then raise your head and push your hips backwards, creating an arch in your back (the 'cat' position).
+                              With each position, extend as far as is comfortable and natural, the goal is not to force extreme range of motion.
                         </div>
                         {/* https://www.youtube.com/watch?v=SKPab2YC8BE&feature=emb_title */}
-                    </div>
-            </div>
-                    <div className="topic">
-                        <div className="item">
-                            <img src={DoorRow} alt='single arm door row' />
-                                <div className="overlay">
-                                SET-UP: Begin in front of a sturdy door frame.
-                                Establish a shoulder width or slightly wider stance and grasp the doorway with one arm so that the wall is perpendicular to your arm.<br />
-                                LIFT: Now slowly sit back using your arm to maintain your upright position and to keep you from falling down.
-                                Once you have a suitable resistance in your arm, simply pull yourself into the doorframe.
-                                Go slowly and do not use momentum.<br />
-                                REPETITIONS: Perform all prescribed reps on one side, then on the other.
-                                This counts as one set.
-                                </div>
-                        </div>
-                    <div className="item">
-                        <img src={endurance} alt='endurance' />
-                            <div className="overlay">
-                                ddhdhdhd dhdhdhdh dhdhdhd hdhdhdh dhdhdh dhh
-                                <br />gdgdgdgdgdgdgdg gdgdgd gdgdgd ggdg
-                                <br />dhdhdhhdhd
-                            </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div>
+
+            <div className="topic">
+                <div className="item"><img src={sideTwist} alt='rotate side to side from torso' />
+                    <div className="overlay">
+                        <div className='overlayText'>
+                            • SET-UP: Begin standing feet apart.
+                            Establish a shoulder width or slightly wider stance.
+                            <br />• MOVEMENT: Now slowly rotate the torso side to side maintaining your upright position.
+                            Reach out to the upper most put of the direction of travel, and return to the lower most on the oppersite side.
+                            <br />• REPETITIONS: Perform all prescribed reps on one side, then on the other.
+                            This counts as one set.
+                        </div>
+                    </div>
+                </div>
+                <div className="item"><img src={wallReach} alt='arms flat to the wall and reach up and repeat' />
+                    <div className="overlay">
+                        <div className='overlayText'>
+                            • SET-UP: Start back against a wall with proper upright posture.
+                            <br /> • MOVEMENT: Pressing the forearms against the wall, and keeping them in contact throughout the movement.
+                            Slide them up until your arms reach full extension. Slide your arms back down, focusing on pulling your shoulder blades together and down.
+                            Bring your elbows into your body and squeeze your shoulder blades together at the bottom of the movement.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div>
+
+                <div className="topic">
+                    <div className="item"><img src={kneelRota} alt='rotation out to the side form kneeling' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                                • SET-UP: Begin on all fours, one hand on the back of your head.<br />
+                                • MOVEMENT: Keeping your core braced and with minimal movement in the lower back.
+                                    Rotate your upper back downwards and bring the elbow of your elevated arm down towards the elbow of your bracing arm.
+                                Slowly reverse the movement and rotate your upper back and arm upwards so that your elbow now points towards the ceiling.<br />
+                                • REPETITIONS: Perform all prescribed reps on one side, then on the other. This counts as one set.
+    
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="item"><img src={pecStretch} alt='pectoral stretch against wall' />
+                        <div className="overlay">
+                            <div className='overlayText'>
+                                • MOVEMENT: Place the upper arm against the side of a door frame or wall.
+                                Now slowly lean your body forward, keeping your shoulder stable and being sure not to rotate your shoulder forward.
+                        To make the stretch more challenging, simply turn your head in the opposite direction.<br />
+                                • REPETITIONS: Hold for the full prescribed time on one side, then switch sides and repeat.
+                                This counts as one set.
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     )
 }
 
