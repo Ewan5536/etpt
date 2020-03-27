61214c0b95275b63f38dbbd3ee3908c2d664a34e
ewan thomas <ewan5536@gmail.com>
2020-03-27 14:06:15 +0000

    next text added

--- src/pages/Motivate.js
+++ src/pages/Motivate.js
@@ -5,10 +5,8 @@ import Iframe from "react-iframe";
 
 function Motiv8() {
     return (
-        <div>
-
-    <Iframe url="https://ewan5536.github.io/MacroCalculator/
-     "
+    <div>
+        <Iframe url="https://ewan5536.github.io/MacroCalculator/"
          width="100% vw"
          height="490px"
          id="myId"
@@ -18,21 +16,39 @@ function Motiv8() {
 
         <div className="overlayText">
             Start Acheiveing Your Goal
+
+                    
+  
+    <h1>The Basics: Metabolism</h1>
+    <h1>The Basics: Metabolism</h1>
+    <p>
+    Find out your BMR (basal metabolic rate) or TDEE (total daily energy expenditure). 
+    </p>
+    <p>
+    Let's say we have a guy named Bob.
+    </p>    Gender: Male<br/>
+    Weight: 163lbs<br/>
+    Height: 5ft 5in<br/>
+    Age: 29<br/>
+    Bodyfat: 14%<br/>
+    BMR: 1746<br/>
+
+    Bob will need to choose his activity level which will be based on how much he's willing to spend time in the gym.<br/> Since he works from home, he can work out 6 times a week while taking a day off to spend time with people he holds dear.
+    <br/>
+    TDEE: 2630<br/>
+    Great! Bob now knows how much calories he needs to maintain his weight.
+    <br/>
+    To lose weight, reduce 500 calories from the TDEE. To gain weight, add 500 calories to TDEE.
+    <br/>
+    Note: For losing weight, I am practitioner of not drastically reducing your caloric intake too much. I would do a 25 minute HIIT cardio on the spin bike that will burn approximately around 250 calories,<br/> and reduce 250 calories from my meal template.
+    <br/>
+  
         </div>
-{/* 
-    refferanced from ewan5536 github account Marcocalc repo https://ewan5536.github.io/MacroCalculator/
-*/}
-</div>
 
-
-
-
-
-
-
-
-
-
+        {/* 
+            refferanced from ewan5536 github account Marcocalc repo https://ewan5536.github.io/MacroCalculator/
+        */}
+    </div>
     )
 }
 export default Motiv8 ;
--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -38,6 +38,132 @@ function Nutrition() {
             allowfullscreen>
           </iframe>
         </div>
+        <div>
+          
+    The Basics: Nutrition<p>
+    You will only need to understand 4 basic macronutrients.
+    </p>
+    Protein - for musculoskeletal development
+    Carbs - muscle recovery
+    Fats - for joints, testosterone and overall hormone health
+    Fiber - controls blood sugar and cholesterol levels among other things
+    There are different approaches to diet that will work for you. For me, I easily get fat if I don't keep my diet in check and I am very prone to binging. Which is why I prefer to go low carb (less than 100g of carbs a day). It helps keep me satiated when I don't frequently eat carbs. But of course, this will vary on the individual. High carb and low fat, or high fat and low carbs? It's up to you.
+    
+    But what is important from it all is the consumption of protein which is key to building of muscles. The most common protein intake would be 1g/lb of your bodyweight. So if you're a 160lb male, then that's 160g of protein per day.
+    
+    Carbs are great for glycogen recovery after a brutal workout which is probably more important than protein during that time. This is why my carbs are placed either at my pre or post workout meal.
+    
+    Meal timing
+    I prefer the bodybuilding diet which is eating every 3-4 hours because I stick with this better.
+    
+    There is also intermittent fasting. Though I have leaned out with it, I don't find its limited timing window as a sustainable way of eating nor a good way for gaining muscles. To each his own.
+    
+    Now back to Bob. Since he weighs 163lbs, he'll need 163g worth of protein each day. So if he plans to eat 6 meals a day, he will need to consume around 27g of protein per meal.
+    
+    Basic Food Choices
+    Protein
+    
+    A large chicken egg without the yolk contains around 3.6g of protein with 17 calories in it (17 calories).
+    150g of boneless chicken breast (weighed after cooked) has around 45g of protein (230 calories).
+    150g of tilapia has 40g of protein, 3g fat (200 calories).
+    150g of lean sirloin filet has 45g of protein and 8g of fat.
+    Fats
+    
+    1 tablesoon of almonds have 13.6g of fat.
+    2tbsp of Skippy's extra crunchy Peanut butter has 16g of fat.
+    15g of bacon has 15g of fats (careful with this one. It's easy to binge on bacons!)
+    Carbs
+    
+    1 cup of brown rice has 45g of carbs which 3.5g of it is fiber (218 calories).
+    1 cup of white rice has 53g of carbs and 0 fiber (242 calories).
+    1 cup or 200g of sweet potato has 41g of carbs, 6.6g of fiber, and 4g of protein (180 calories).
+    1 cup of oats has 25g of carbs, 7g protein. (88 calories).
+    I place all lean protein like chicken and eggs during the morning just because I consume carbs before I workout. Fats will be placed after the post workout meal. Foods like pangasius, tilapia, bangus, sirloin, etc. Just my preference. I feel satiated when I consume fats and protein which prevents me from reaching the phone and ordering a quarter pounder with fries from mcdonald's at 11pm in th evening.
+    
+    Cheat meals
+    Cheat meals (not binging) are great to systematically help yourself stick to your diet. The calories in cheat meals should still be within your TDEE. But keep in mind that if you're a fatty, you will just be pushing yourself further from your goals when you cheat. That said, my preferred cheat meals are:
+    
+    Samanco - Vanilla Ice Cream Sandwich W/Red Bean Paste - 274 calories, 35g carbs, 6g protein
+    1 standard Futo Maki roll - 380 calories, 72g carbs, 7g protein, 4.5g fat
+    1 standard burger patty with no buns - 22g protein, 230 calories
+  <br/>The Basics: Training
+      There 4 basic lifts that I believe helps contribute to overall physical strength and fitness.
+      
+      Bench Press
+      Squats
+      Deadlifts
+      Overhead press
+      Bench press is great for upper body development in the pectorial, anterior deltoids and tricep area. When you see someone with a well developed upper-body, chances are he's got a pretty good bench press number.
+      
+      Deadlifts contribute to overall back thickness. It has three types - conventional, stiff-legged, and sumo. Conventional is the most common which uses the lower back and glutes to lift the bar from the ground. The stiff-legged deadlift has more hamstrings actions. And lastly, sumo uses the adductors and hamstrings to stabilize the form.
+      
+      Squat is probably the exercise that anyone should really focus on. It develops overall core and lower body development. It's the ultimate exercise that triggers the body's need to get stronger.
+      
+      Strict Overhead Press (or simply OHP) focuses more on delts, traps, and overall shoulder development. It's the hardest exercise to perform among the three in my opinion since a strict OHP form prohibits the bouncing of your body to cheat lift the bar up.
+      
+      Training for Strength vs Hypertrophy
+      There are two basic approaches to lifting weights - strength and (muscle) size.
+      
+      Training for strength will give size over time while training for size will give strength over time. Meaning if you spend 6 months trying to increase your strength, don't expect to have noticeable changes in terms of muscularity.
+      
+      This is the point where you'll have to choose which type of pokemon you want to be a year or two from now. But as for any craft that requires discipline like art, programming, designing, et.al – the most important thing is your foundation.
+      
+      For me it requires tremendous amount of practice in order to increase mastery at something. For one to become a great painter, one must stroke the pen with proper form over a long period of time to fully create that mind and pen connection.
+      
+      Likewise in lifting weights, the best way to become adept at a certain movement is to practice it very often. And that requires repeating the same movement over and over again.
+      
+      Using this philosophy, I will be providing a hypertrophy-based routine.
+      
+      Some notes before we begin:
+      
+      An exercise with a label like 3x12 means 3 sets of 12 reps. If you're able to perform those 12 reps with perfect form, you should increase the weights in the next scheduled day of exercise. If you can't perform at least 8 reps of that exercise, reduce the weight.
+      Rest between sets should be around 60-90 seconds. Important thing is to pump the blood into the muscle and stretch the fiber.
+      Day 1: Push movements
+      Overhead Press (OHP) 3x12
+      Dumbbell shoulder press 3x12
+      Incline Dumbbell press 3x12
+      Barbell bench press 3x12
+      Close-grip bench press 3x12
+      Skull crusher using the ez-bar 3x12
+      Tricep cable push down 3x12
+      Day 2: Pull movements
+      Lat pull downs (wide-grip) 3x12
+      Close grip lat pull downs 3x12
+      Tbar rows or cable rows 3x12
+      One arm dumbbell rows 3x12
+      Barbell rows 3x12
+      Overhand grip barbell rows 3x12
+      Deadlift - 5x5
+      Barbell curls 3x10
+      Preacher curls 3x10
+      Hammer curls 3x10
+      Day 3: Leg movements
+      Squats conventional 3x8
+      Leg extensions 3x12
+      Leg curls 3x12
+      Stiff leg deadlift 3x10
+      Calf raises 3x20
+      Decline crunches 3x10
+      Sample Training Schedule
+      Regular
+      
+      Mon - Push
+      Tue - REST
+      Wed - Pull
+      Thu - REST
+      Fri - Legs
+      Sat - REST
+      Sun - REST
+      Advanced
+      
+      Mon - Push
+      Tue - Pull
+      Wed - Legs
+      Thu - Push
+      Fri - Pull
+      Sat - Legs
+      Sun - REST
+        </div>
         </>
     );
 }
