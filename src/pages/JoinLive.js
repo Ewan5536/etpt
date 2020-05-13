5f7499431d5c6c3b1a0828d2cfae303c17a62a08
ewan thomas <ewan5536@gmail.com>
2020-05-13 21:22:16 +0100

    new stream service

--- src/pages/JoinLive.js
+++ src/pages/JoinLive.js
@@ -28,9 +28,10 @@ function JoinLive() {
                     Sunday: By Appointment Only
                 </div>
             </div>
-        </div>
+        </div>        
         <div>
-            <Iframe url="https://play.streamingvideoprovider.com/popplayer.php?it=pmci5tze9c0w&is_link=1&w=720&h=405&pause=1&title=ET-PT+Live+Classes&skin=3&repeat=&brandNW=1&start_volume=100&bg_gradient1=%23ffffff&bg_gradient2=%23e9e9e9&fullscreen=1&fs_mode=2&skinAlpha=50&colorBase=%23000000&colorIcon=%23ffffff&colorHighlight=%235cbbf5&direct=false&no_ctrl=&auto_hide=1&viewers_limit=0&cc_position=bottom&cc_positionOffset=70&cc_multiplier=0.03&cc_textColor=%23ffffff&cc_textOutlineColor=%23ffffff&cc_bkgColor=%23000000&cc_bkgAlpha=0.1&image=&mainBg_Color=%23ffffff&aspect_ratio=16%3A9"
+            
+            <Iframe url="https://livewebinar.com/467-487-535/b0454516fd26db4b88012fc25de5d3cc"
                 width="100% vw"
                 height="490px"
                 id="myId"
