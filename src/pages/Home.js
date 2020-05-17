def758c20ac511a3f049af3b2de766867ea40fed
ewan thomas <ewan5536@gmail.com>
2020-05-17 08:10:58 +0100

    text edit on home pg.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import work40 from '../images/work40.png';
 import endurCrd from '../images/endurCard.gif';
 import strength from '../images/strenCrd.png';
 import flexability from '../images/flexCrd.png';
-
+/*import images*/
 import { Link } from 'react-router-dom'
 
 function Home () {
@@ -20,33 +20,33 @@ function Home () {
           </Link>
         </div>
 
-        {/* gif image for homepage routing */}
+        {/* gif image perform routing */}
         <div className="item">
           <Link to='/strength'>
           <img src={strength} alt='strength' />
-          <button className="overlay">Strength Training</button></Link>
+          <button className="overlay">Start Strength Training</button></Link>
         </div>
         <div className="item">
           <Link to='/flexability'>
           <img src={flexability} alt='flexability' />
-          <button className="overlay">Flexabiltity Training</button></Link>
+          <button className="overlay">Start Flexabiltity Training</button></Link>
         </div>
       </div>
       <div className="topic">
         <div className="item">
           <Link to='/10minWo'>
           <img src={work10} alt='10min routine' />
-          <button className="overlay">10min Training</button></Link>
+          <button className="overlay">Start 10min Training</button></Link>
         </div>
         <div className="item">
           <Link to='/20minWo'>
           <img src={work20} alt='20min routine' />
-          <button className="overlay">20min Training</button></Link>
+          <button className="overlay">Start 20min Training</button></Link>
         </div>
         <div className="item">
           <Link to='/40minWo'>
           <img src={work40} alt='40min routine' />
-          <button className="overlay">40min Training</button></Link>
+          <button className="overlay">Start 40min Training</button></Link>
         </div>
       </div>
 
